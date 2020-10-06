dbc3433ee4ca7526a0aeed543a77dc0daacddb1c
bartosian <bartosian1989@gmail.com>
2020-10-06 19:46:03 -0400

    Added apollo server to express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-// index.js
-// This is the main entry point of our application
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
 
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello, world'));
-app.listen(4000, () =>
-  console.log(`Server running at http://localhost:${port}`)
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
