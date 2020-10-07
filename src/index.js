ceec58095bd909a95673f76b13842c632bb34bb5
bartosian <bartosian1989@gmail.com>
2020-10-07 10:02:33 -0400

    Added authentication withh tokenization to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const models = require('./models');
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
@@ -15,12 +16,27 @@ const app = express();
 
 db.connect(DB_HOST);
 
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invalid');
+    }
+  }
+};
+
 // Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+
+    console.log(user);
+
+    return { models, user };
   }
 });
 
--- src/schema.js
+++ src/schema.js
@@ -11,12 +11,21 @@ module.exports = gql`
     newNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
+  }
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
   }
 
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
   }
