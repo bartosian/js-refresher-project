d9a54a4e0d87e08dfb5617967d75deec92791b61
bartosian <bartosian1989@gmail.com>
2020-10-07 09:17:33 -0400

    Added CRUD mutations to apollo server

--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  scalar DateTime
   type Query {
     notes: [Note!]!
     note(id: ID!): Note!
@@ -8,11 +9,15 @@ module.exports = gql`
 
   type Mutation {
     newNote(content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
+    deleteNote(id: ID!): Boolean!
   }
 
   type Note {
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 `;
